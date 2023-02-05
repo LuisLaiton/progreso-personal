a51eb6531b551e0cca0f245d26d74bae1a9ffef0
LuisLaiton <50175115+LuisLaiton@users.noreply.github.com>
2023-02-05 13:20:25 -0500

    Base de pinia y composable

---
+++ src/store/counter.js
@@ -0,0 +1,10 @@
+import { defineStore } from 'pinia'
+
+export const useCounterStore = defineStore('id', () => {
+    // Estados
+    // Acciones
+    // Getters
+    return {
+        count
+    }
+})
