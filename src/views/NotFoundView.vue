74665e60adcbb2d8ea003de5873c2359148204e5
LuisLaiton <50175115+LuisLaiton@users.noreply.github.com>
2023-02-05 13:35:58 -0500

    Pagina de error

---
+++ src/views/NotFoundView.vue
@@ -0,0 +1,16 @@
+<script setup>
+import { useRouter } from 'vue-router'
+
+const router = useRouter()
+
+const back = () => {
+    router.push('/')
+}
+</script>
+
+<template>
+    <div class="text-center my-5">
+    <h1 class="text-danger fw-bold display-1">404</h1>
+    <button @click="back" class="btn btn-outline-danger">Volver al inicio</button>
+    </div>
+</template>
